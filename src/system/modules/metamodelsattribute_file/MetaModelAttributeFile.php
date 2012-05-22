88bd39a0cccb9cabf22463a8c77fb716ec381687
Christian Schiffler <c.schiffler@cyberspectrum.de>
2012-05-22 21:06:05 +0200

    Initial check in. Base interfaces should be pretty much finished but still require cleanup and more "flesh" regarding filtering and transforming into output data.

---
+++ src/system/modules/metamodelsattribute_file/MetaModelAttributeFile.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The MetaModels extension allows the creation of multiple collections of custom items,
+ * each with its own unique set of selectable attributes, with attribute extendability.
+ * The Front-End modules allow you to build powerful listing and filtering of the
+ * data in each collection.
+ *
+ * PHP version 5
+ * @package	   MetaModels
+ * @subpackage AttributeFile
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @copyright  CyberSpectrum
+ * @license    private
+ * @filesource
+ */
+if (!defined('TL_ROOT'))
+{
+	die('You cannot access this file directly!');
+}
+
+/**
+ * This is the MetaModelAttribute class for handling file fields.
+ * 
+ * @package	   MetaModels
+ * @subpackage AttributeFile
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ */
+class MetaModelAttributeFile extends MetaModelAttributeSimple
+{
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function getSQLDataType()
+	{
+		return 'varchar(255) NOT NULL default \'\'';
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public static function getFieldSettingNames()
+	{
+		return array_merge(parent::getFieldSettingNames(), array(
+			'parentCheckbox',
+			'titleField',
+			'width50',
+			'insertBreak',
+			'sortingField',
+			'filteredField',
+			'searchableField',
+			'mandatory',
+			'defValue',
+			'uniqueItem',
+			'formatPrePost',
+			'format',
+			'editGroups'
+		));
+	}
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function getFieldDefinition()
+	{
+		$arrFieldDef=parent::getFieldDefinition();
+		$arrFieldDef['inputType'] = 'fileTree';
+		return $arrFieldDef;
+	}
+}
+
+?>
---
+++ src/system/modules/metamodelsattribute_file/dca/tl_metamodel_attribute.php
@@ -0,0 +1,83 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+/**
+ * Table tl_metamodel_attribute 
+ */
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['metapalettes']['file extends _simplefield_'] = array
+(
+	'+advanced' => array('file_showImage', 'file_customFiletree', 'file_multiple'),
+	'+backenddisplay'	=> array('-width50'),
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_customFiletree'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'],
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true, 'tl_class'=>'clr')
+);
+
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_multiple'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['multiple'],
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true, 'tl_class'=>'clr')
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_sortBy'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['sortBy'],
+	'exclude'                 => true,
+	'inputType'               => 'select',
+	'options'                 => array('name_asc', 'name_desc', 'date_asc', 'date_desc', 'meta', 'random'),
+	'reference'               => &$GLOBALS['TL_LANG']['tl_metamodel_attribute'],
+	'eval'                    => array('tl_class'=>'w50')
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_showLink'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['showLink'],
+	'inputType'               => 'checkbox',
+	'eval'                    => array('tl_class'=>'w50 m12')
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_showImage'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_showImage'],
+	'inputType'               => 'checkbox',
+	'eval'                    => array('submitOnChange'=>true) 
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_imageSize'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['imageSize'],
+	'exclude'                 => true,
+	'inputType'               => 'imageSize',
+	'options'                 => array('crop', 'proportional', 'box'),
+	'reference'               => &$GLOBALS['TL_LANG']['MSC'],
+	'eval'                    => array('rgxp'=>'digit', 'nospace'=>true, 'tl_class'=>'w50')
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_uploadFolder'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['uploadFolder'],
+	'exclude'                 => true,
+	'inputType'               => 'fileTree',
+	'eval'                    => array('fieldType'=>'radio', 'tl_class'=>'clr')
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_validFileTypes'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['validFileTypes'],
+	'inputType'               => 'text',
+	'eval'                    => array('maxlength'=>255, 'tl_class'=>'w50')
+);
+
+$GLOBALS['TL_DCA']['tl_metamodel_attribute']['fields']['file_filesOnly'] = array
+(
+	'label'                   => &$GLOBALS['TL_LANG']['tl_metamodel_attribute']['filesOnly'],
+	'inputType'               => 'checkbox',
+	'eval'                    => array('tl_class'=>'w50 m12')
+);
+
+?>
