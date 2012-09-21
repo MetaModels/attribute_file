0328bb30a8b788b7993ffe10311496143b6ed76c
Stefan heimes <stefan_heimes@hotmail.com>
2012-09-21 18:51:13 +0200

    Update language files

--- src/system/modules/metamodelsattribute_file/languages/en/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_file/languages/en/tl_metamodel_attribute.php
@@ -21,12 +21,13 @@ if (!defined('TL_ROOT'))
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'] = array('Customize the file tree', 'Allows you to set custom options for the Filetree.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']   = array('Set file root folder', 'Selects the root point from which the user will select this file field.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes'] = array('Valid file types', 'Please enter a comma separated list of extensions of valid file types for this field.');
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']      = array('Allow files only', 'Select this option to restrict the file browser to files only (folders not selectable).');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['file']        = 'File';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']        = array('Customize the file tree', 'Allows you to set custom options for the Filetree.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']          = array('Set file root folder', 'Selects the root point from which the user will select this file field.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']        = array('Valid file types', 'Please enter a comma separated list of extensions of valid file types for this field.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']             = array('Allow files only', 'Select this option to restrict the file browser to files only (folders not selectable).');
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']       = array('Multiple selection', 'If selected, user will be able to select more than one item.');
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']              = array('Multiple selection', 'If selected, user will be able to select more than one item.');
 
 
 ?>
