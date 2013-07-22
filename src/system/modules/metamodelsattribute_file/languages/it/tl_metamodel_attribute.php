5c8f746c4ce711843ec848a2a9aa4910b4c11b5d
Mini Model <minimodel@metamodel.me>
2013-07-22 12:43:15 +0200

    Updated translations from transifex.

--- src/system/modules/metamodelsattribute_file/languages/it/tl_metamodel_attribute.php
+++ src/system/modules/metamodelsattribute_file/languages/it/tl_metamodel_attribute.php
@@ -8,8 +8,17 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2013-05-13T07:58:28+02:00
+ * last-updated: 2013-07-10T10:04:24+02:00
  */
 
-
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['file'] = 'File';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['0'] = 'Personalizza la struttura ad albero dei file';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['1'] = 'Questa opzione permette di impostare le opzioni personalizzate per la gestione file.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['0']      = 'Abilita solo file';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['1']      = 'Seleziona questa opzione per limitare la selezione ai soli file (le cartelle non saranno selezionabili)';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['0']       = 'Selezione multipla';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['1']       = 'Selezionare questa opzione se si desidera selezionare più di un elemento.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['0']   = 'Imposta la cartella principale per i file';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['1']   = 'Seleziona la cartella principale che sarà utilizzata per la selezione dei campi file';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['0'] = 'Tipologia file validi';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['1'] = 'Inserire i tipi di file che si vuole visualizzare. Utilizzare il simbolo pipe come separatore.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['file']      = 'File';
