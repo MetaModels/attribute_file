0f5147c1c26480a4db69c04a4bae756adc46ed1e
Stefan Heimes <stefan_heimes@hotmail.com>
2013-09-17 00:03:52 +0200

    Add support for contao 3 and DBAFS.

--- src/system/modules/metamodelsattribute_file/MetaModels/Attribute/File/File.php
+++ src/system/modules/metamodelsattribute_file/MetaModels/Attribute/File/File.php
@@ -132,12 +132,26 @@ class File extends BaseSimple
 			{
 				foreach ($arrRowData[$this->getColName()] as $strFile)
 				{
-					$objToolbox->addPath($strFile);
+					if (version_compare(VERSION, '3.0', '<'))
+					{
+						$objToolbox->addPath($strFile);
+					}
+					else
+					{
+						$objToolbox->addPathById($strFile);
+					}
 				}
 			}
 			else
 			{
-				$objToolbox->addPath($arrRowData[$this->getColName()]);
+				if (version_compare(VERSION, '3.0', '<'))
+				{
+					$objToolbox->addPath($arrRowData[$this->getColName()]);
+				}
+				else
+				{
+					$objToolbox->addPathById($arrRowData[$this->getColName()]);
+				}
 			}
 		}
 
