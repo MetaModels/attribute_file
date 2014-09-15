4e5119c789f4356bb12408b5c1988a0bc9e0a7e0
Christian Schiffler <c.schiffler@cyberspectrum.de>
2014-09-15 23:47:58 +0200

    Fix file headers.

--- src/system/modules/metamodelsattribute_file/languages/en/tl_metamodel_rendersetting.php
+++ src/system/modules/metamodelsattribute_file/languages/en/tl_metamodel_rendersetting.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The MetaModels extension allows the creation of multiple collections of custom items,
  * each with its own unique set of selectable attributes, with attribute extendability.
@@ -7,6 +6,7 @@
  * data in each collection.
  *
  * PHP version 5
+ *
  * @package    MetaModels
  * @subpackage AttributeFile
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
