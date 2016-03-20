fbad4c4152f5ecb07b6a05977d10bbc73f094a2d
Mini Model <minimodel@metamodel.me>
2016-03-21 00:16:44 +0100

    Updated translations from transifex.

--- contao/languages/fr/tl_metamodel_attribute.php
+++ contao/languages/fr/tl_metamodel_attribute.php
@@ -8,17 +8,19 @@
  *
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  *
- * last-updated: 2014-05-08T05:14:27+02:00
+ * last-updated: 2015-07-25T00:10:50+02:00
  */
 
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['0'] = 'Personnalier l\'arborescence de fichiers';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['0'] = 'Personnaliser l\'arborescence de fichiers';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree']['1'] = 'Vous permet de choisir les réglages personnalisés concernant l\'arborescence des fichiers';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filePicker']['0']     = 'Sélecteur de fichiers';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['0']      = 'Autoriser seulement les fichiers';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['1']      = 'Sélectionner cette option pour restreindre l\'explorateur au fichier seulement (répertoires non sélectionnables)';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly']['1']      = 'Sélectionner cette option pour restreindre l\'explorateur aux fichiers seulement (répertoires non sélectionnables)';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['0']       = 'Sélection multiple';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['1']       = 'Si sélectionné, l\'utilisateur sera capable de sélectionner plus d\'un champ.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple']['1']       = 'Si sélectionné, l\'utilisateur pourra sélectionner plus d\'un élément.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['0']   = 'Définit le répertoire racine';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['1']   = 'Sélectionne le point racine à partir duquel l\'utilisateur sélectionnera le champ du fichier';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder']['1']   = 'Sélectionne le répertoire racine à partir duquel l\'utilisateur pourra naviguer pour sélectionner les fichiers du champs.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['0'] = 'Type de fichiers valides';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['1'] = 'Veuillez entrer une liste séparée par des virgules des types de fichiers valide pour ce champ.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes']['1'] = 'Si nécessaire, entrez la liste des types de fichiers valide pour ce champs séparés par des virgules. Sinon, la liste par défaut de Contao sera utilisée.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['file']      = 'Fichier';
+
