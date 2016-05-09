977ad4505bf1c2796d90e64e2b5e616e01f7e70f
Christian Schiffler <c.schiffler@cyberspectrum.de>
2016-05-09 18:14:07 +0200

    Fix phpcs issues

--- contao/languages/en/tl_metamodel_attribute.php
+++ contao/languages/en/tl_metamodel_attribute.php
@@ -1,28 +1,38 @@
 <?php
+
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_file.
  *
- * PHP version 5
+ * (c) 2012-2015 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
  * @package    MetaModels
  * @subpackage AttributeFile
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
 // Fields.
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['typeOptions']['file']    = 'File';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'][0] = 'Customize the file tree';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'][1] = 'Allows you to set custom options for the filetree.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_customFiletree'][1] =
+    'Allows you to set custom options for the filetree.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder'][0]   = 'Set file root folder';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder'][1]   = 'Selects the root point from which the user will select this file field.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_uploadFolder'][1]   =
+    'Selects the root point from which the user will select this file field.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes'][0] = 'Valid file types';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes'][1] = 'To overwrite the contao standard file types, please enter a comma separated list of extensions of valid file types for this field.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_validFileTypes'][1] =
+    'To overwrite the contao standard file types, please enter a comma separated list of extensions of valid file ' .
+    'types for this field.';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly'][0]      = 'Allow files only';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly'][1]      = 'Select this option to restrict the file browser to files only (folders not selectable).';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_filesOnly'][1]      =
+    'Select this option to restrict the file browser to files only (folders not selectable).';
 $GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple'][0]       = 'Multiple selection';
-$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple'][1]       = 'If selected, user will be able to select more than one item.';
+$GLOBALS['TL_LANG']['tl_metamodel_attribute']['file_multiple'][1]       =
+    'If selected, user will be able to select more than one item.';
--- contao/languages/en/tl_metamodel_rendersetting.php
+++ contao/languages/en/tl_metamodel_rendersetting.php
@@ -1,29 +1,36 @@
 <?php
+
 /**
- * The MetaModels extension allows the creation of multiple collections of custom items,
- * each with its own unique set of selectable attributes, with attribute extendability.
- * The Front-End modules allow you to build powerful listing and filtering of the
- * data in each collection.
+ * This file is part of MetaModels/attribute_file.
  *
- * PHP version 5
+ * (c) 2012-2015 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
  *
  * @package    MetaModels
  * @subpackage AttributeFile
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  The MetaModels team.
- * @license    LGPL.
+ * @copyright  2012-2016 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
-// Fields.
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showImage'][0] = 'Enable as image field with thumbnail';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showImage'][1] = 'If selected, a thumbnail will be created for image files.';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showImage'][1] =
+    'If selected, a thumbnail will be created for image files.';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_sortBy'][0]    = 'Order by';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_sortBy'][1]    = 'Please choose the sort order.';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showLink'][0]  = 'Create link as file download or image lightbox';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showLink'][1]  = 'Wraps the item in a link that will show the fullscreen image or download the file.';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showLink'][0]  =
+    'Create link as file download or image lightbox';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_showLink'][1]  =
+    'Wraps the item in a link that will show the fullscreen image or download the file.';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_imageSize'][0] = 'Image width and height';
-$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_imageSize'][1] = 'Please enter either the image width, the image height or both measures to resize the image. If you leave both fields blank, the original image size will be displayed.';
+$GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['file_imageSize'][1] =
+    'Please enter either the image width, the image height or both measures to resize the image. If you leave both ' .
+    'fields blank, the original image size will be displayed.';
 
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['name_asc']  = 'File name (ascending)';
 $GLOBALS['TL_LANG']['tl_metamodel_rendersetting']['name_desc'] = 'File name (descending)';
