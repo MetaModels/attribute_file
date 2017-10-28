5755d46befe9f565f62dea7452c668122203d4c5
Sven Baumann <baumann.sv@gmail.com>
2017-10-28 10:11:26 +0200

    Change from module to bundle

---
+++ src/MetaModelsAttributeFileBundle.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of MetaModels/attribute_file.
+ *
+ * (c) 2012-2017 The MetaModels team.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * This project is provided in good faith and hope to be usable by anyone.
+ *
+ * @package    MetaModels
+ * @subpackage AttributeFile
+ * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
+ * @author     Sven Baumann <baumann.sv@gmail.com>
+ * @copyright  2012-2017 The MetaModels team.
+ * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0
+ * @filesource
+ */
+
+namespace MetaModels\Attribute\File;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * This is the bundle class.
+ */
+class MetaModelsAttributeFileBundle extends Bundle
+{
+}
