1076e5b5f5cdd3268aae809be80ea1e67e591fd9
Ingolf Steinhardt <info@e-spin.de>
2020-02-27 15:35:53 +0100

    ad manual file sorting

--- src/Resources/contao/dca/tl_metamodel_dcasetting.php
+++ src/Resources/contao/dca/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_file.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -15,7 +15,8 @@
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Andreas Isaak <info@andreas-isaak.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -25,6 +26,7 @@ $GLOBALS['TL_DCA']['tl_metamodel_dcasetting']['metasubselectpalettes']['attr_id'
         'tl_class'
     ],
     'functions'    => [
-        'mandatory'
+        'mandatory',
+        'file_widgetMode'
     ]
 ];
--- src/Resources/contao/languages/en/tl_metamodel_dcasetting.php
+++ src/Resources/contao/languages/en/tl_metamodel_dcasetting.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_file.
  *
- * (c) 2012-2016 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,17 +13,19 @@
  * @package    MetaModels
  * @subpackage AttributeFile
  * @author     Christian Schiffler <c.schiffler@cyberspectrum.de>
- * @copyright  2012-2016 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0
  * @filesource
  */
 
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['file_widgetMode'][0] = 'Widget mode';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['file_widgetMode'][1] =
-    'If selected, the files will show as a (sortable) image gallery.';
+    'With the mode the display type can be selected.';
 
-$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['file_widgetModes']['normal']    = 'Show selected files only.';
+$GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['file_widgetModes']['normal']    =
+    'Show files as list.';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['file_widgetModes']['downloads'] =
-    'Expand folders as (sortable) file list.';
+    'Show files as sortable file list e.g. for downloads.';
 $GLOBALS['TL_LANG']['tl_metamodel_dcasetting']['file_widgetModes']['gallery']   =
-    'Expand folders as (sortable) image gallery.';
+    'Show files as sortable images e.g. for gallery.';
