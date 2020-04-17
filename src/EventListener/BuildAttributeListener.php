bff585b8640c4d095fd8f9774fd5b440fefb419b
Tsarma <tsarma@users.noreply.github.com>
2020-04-17 13:49:27 +0200

    Update BuildAttributeListener.php

--- src/EventListener/BuildAttributeListener.php
+++ src/EventListener/BuildAttributeListener.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_file.
  *
- * (c) 2012-2019 The MetaModels team.
+ * (c) 2012-2020 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -13,7 +13,8 @@
  * @package    MetaModels/attribute_file
  * @author     Stefan Heimes <stefan_heimes@hotmail.com>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2019 The MetaModels team.
+ * @author     Tenzin Tsarma <git@tsarma.ch>
+ * @copyright  2012-2020 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
