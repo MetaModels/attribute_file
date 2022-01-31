5aec65e1133d916552d4af28b6285a835aecff23
Ingolf Steinhardt <git@e-spin.de>
2022-01-31 14:03:13 +0100

    Add private download option

--- src/Attribute/File.php
+++ src/Attribute/File.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of MetaModels/attribute_file.
  *
- * (c) 2012-2021 The MetaModels team.
+ * (c) 2012-2022 The MetaModels team.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -22,7 +22,8 @@
  * @author     Marc Reimann <reimann@mediendepot-ruhr.de>
  * @author     David Molineus <david.molineus@netzmacht.de>
  * @author     Sven Baumann <baumann.sv@gmail.com>
- * @copyright  2012-2021 The MetaModels team.
+ * @author     Ingolf Steinhardt <info@e-spin.de>
+ * @copyright  2012-2022 The MetaModels team.
  * @license    https://github.com/MetaModels/attribute_file/blob/master/LICENSE LGPL-3.0-or-later
  * @filesource
  */
@@ -562,6 +563,7 @@ class File extends BaseComplex
             $toolbox->addPathById($value);
         }
 
+        $toolbox->withDownloadKeys((bool) $settings->get('file_downloadKey'));
         $toolbox->resolveFiles();
         $data = $toolbox->sortFiles($settings->get('file_sortBy'), ($value['bin_sorted'] ?? []));
 
